8fab66d33ef2099da3e3ea87afb63d46f7c03c9d
PedroLuisBernardos <kk391035@ens.uqam.ca>
2020-07-31 11:12:43 -0400

    tempo quest mage

--- html/tempo_quest_mage/semibudget.js
+++
@@ -1,6 +0,0 @@
-function clicToCopySemiBudget() {
-    var copyText = document.getElementById("deckCodeSemiBudget");
-    copyText.select();
-    copyText.setSelectionRange(0, 99999)
-    document.execCommand("copy");
-}
