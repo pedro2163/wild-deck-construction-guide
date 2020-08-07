2f6a241e01ecf67608bdc581ad941ace9bbe4755
PedroLuisBernardos <kk391035@ens.uqam.ca>
2020-08-07 15:29:55 -0400

    modif

--- html/reno_paladin/budget.js
+++
@@ -1,6 +0,0 @@
-function clicToCopyBudget() {
-    var copyText = document.getElementById("deckCodeBudget");
-    copyText.select();
-    copyText.setSelectionRange(0, 99999)
-    document.execCommand("copy");
-}
--- html/reno_paladin/final.js
+++
@@ -1,6 +0,0 @@
-function clicToCopyFinal() {
-    var copyText = document.getElementById("deckCodeFinal");
-    copyText.select();
-    copyText.setSelectionRange(0, 99999)
-    document.execCommand("copy");
-}
